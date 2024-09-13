a7c49219933bccfae2faf137367b4ce1f5322a92
cukminseo <kms39273@catholic.ac.kr>
2024-09-13 13:05:32 +0900

    [FEAT] 폴더 생성방법 변경

--- utils/utils.py
+++ utils/utils.py
@@ -2,19 +2,15 @@ import os
 from datetime import datetime
 
 
-# 실험마다 새로운 폴더에 보관하기 위한 기능
-## experiments 폴더 내부에 하위 폴더를 생성하고 path를 전달합니다
 def create_experiment_folder(CFG, base_path="./experiments"):
     # 현재 시간 기록
-    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-    # 날짜와 시간만 표시
-    date_and_hour = current_time[5:13]
+    current_time = datetime.now().strftime("%m%d_%H%M")
 
     # admin 값을 가져와서 폴더 이름에 추가
-    user_name = CFG['user_name']
+    user_name = CFG["user_name"]
 
-    experiment_folder_name = f"{date_and_hour}_{user_name}"
+    # 월일_시간분_user_name 형식으로 폴더 이름 생성
+    experiment_folder_name = f"{current_time}_{user_name}"
 
     # experiments 경로에 해당 폴더 생성
     experiment_path = os.path.join(base_path, experiment_folder_name)
