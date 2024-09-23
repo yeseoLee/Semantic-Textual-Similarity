ed99ec84d90ee18ca47a7efdbf758b7e168aa82b
cukminseo <kms39273@catholic.ac.kr>
2024-09-23 15:30:07 +0900

    [Refactor] 단일책임원칙에 의한 함수분리

--- utils/utils.py
+++ utils/utils.py
@@ -2,20 +2,28 @@ import os
 from datetime import datetime
 
 
-def create_experiment_folder(CFG, base_path="./experiments"):
+def get_experiment_folder_name(CFG):
     # 현재 시간 기록
     current_time = datetime.now().strftime("%m%d_%H%M")
 
-    # admin 값을 가져와서 폴더 이름에 추가
+    # CFG 값을 가져와서 폴더 이름에 추가
     user_name = CFG["user_name"]
     model_name = CFG["model"]["model_name"]
     lr = CFG["train"]["learning_rate"]
     batch_size = CFG["train"]["batch_size"]
 
-    # 월일_시간분_user_name 형식으로 폴더 이름 생성
+    # 폴더 이름 생성
     experiment_folder_name = (
         f"{current_time}_{model_name}_lr{lr}_batch{batch_size}({user_name})"
     )
+
+    return experiment_folder_name
+
+
+def create_experiment_folder(CFG, base_path="./experiments"):
+
+    # 월일_시간분_user_name 형식으로 폴더 이름 생성
+    experiment_folder_name = get_experiment_folder_name(CFG)
 
     # experiments 경로에 해당 폴더 생성
     experiment_path = os.path.join(base_path, experiment_folder_name)
